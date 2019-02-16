286f9bbe15b63f308240696d81f7e86c5c739e4e
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-02-16 17:43:24 -0500

    Create quadradic.java

---
+++ quadradic.java
@@ -0,0 +1,29 @@
+// Program that evaluates a quadradic equation
+
+import java.util.Scanner;
+import java.lang.Math; 
+
+class practice 
+	{
+		public static void main(String[] args)
+		{
+			Scanner input = new Scanner(System.in); 
+			
+			double a, b, c; 
+			double QFormula; 
+			
+			System.out.print("Input a value for a: ");
+			a = input.nextDouble();
+			
+			System.out.print("Input a value for b: ");
+			b = input.nextDouble(); 
+			
+			System.out.print("Input a value for c: ");
+			c = input.nextDouble(); 
+			
+			QFormula = (- b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+			
+			System.out.println("The answer is: " + QFormula);
+			
+		}
+	}	
