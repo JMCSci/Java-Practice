aa2a9d92dbdf7e04baf23a97bd877b9c02468fe7
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-03-01 18:48:18 -0500

    Create weight.java

---
+++ weight.java
@@ -0,0 +1,18 @@
+// Program expresses weight in pounds and displays it terms of pounds and ounces
+// Example: 5.2 pounds = 5 pounds 3 ounces
+
+import java.util.Scanner;
+
+class practice 
+	{
+		public static void main(String[] args)
+		{
+			Scanner input = new Scanner(System.in);
+			double weight, decimal, ounces;
+			System.out.print("Please enter weight: ");
+			weight = input.nextDouble(); 
+			decimal = weight - Math.floor(weight); // leaves numbers after decimal
+			ounces = (decimal * 16);
+			System.out.println("You weigh " + (int)(weight) + " pounds and " + ounces + " ounces"); 
+		}
+	}
