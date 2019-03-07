a925322df08d8c903664323cd5ba2f6ed77aa70c
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-03-07 17:46:52 -0500

    Update salestax.java

--- salestax.java
+++ salestax.java
@@ -2,21 +2,21 @@ package practice2;
 
 import java.util.Scanner;
 
-// practice functions/methods
+// practice functions
 
 public class practice2 {
 
 	public static double salesTax(double tax, double x) {
-		tax = 6.875;
+		tax = 8.875;
 		return x / tax; 
 	}
 	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		double price, total;
-		System.out.print("Input a price: ");
+		System.out.print("Enter the price of product: ");
 		price = input.nextDouble(); 
 		total = (salesTax(0,price) + price);
-		System.out.printf("The price with taxes is $%.2f.", total);
+		System.out.printf("The cost with sales tax is $%.2f.", total);
 	}
 }
