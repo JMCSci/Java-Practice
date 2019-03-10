29dfccec05cecd001f18db726dc2b04b276299db
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-03-09 19:02:30 -0500

    Create string.java

---
+++ string.java
@@ -0,0 +1,15 @@
+package practice2;
+
+import java.util.Scanner;
+
+// practice String
+
+public class practice2 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String name;
+		System.out.print("Enter your name: ");
+		name = input.nextLine(); 
+		System.out.println("Hello " + name + "");	
+	}
+}
