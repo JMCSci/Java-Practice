74fdb3f5a95aa19abc240e9ef6cc00cb478faf41
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-03-21 10:59:08 -0400

    Create methods2.java

---
+++ methods2.java
@@ -0,0 +1,22 @@
+package practice2;
+
+import java.util.Scanner;
+
+// call a method to use as input 
+
+public class practice2 {
+	
+	public static String firstName(String a) {
+		Scanner input = new Scanner(System.in);
+		String b = input.next();
+		input.close();
+		return b;
+	}
+	
+	public static void main(String[] args) {
+		String x = "NULL";  
+		System.out.println("Enter your first name: ");
+		System.out.print(firstName(x));
+		
+	}
+}
