5cc6997fb57f2845c633c4af09b3ecaa831908e6
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-04-14 18:17:53 -0400

    Update array4(sort).java

--- array4(sort).java
+++ array4(sort).java
@@ -14,7 +14,7 @@ public class practice2 {
 			array[i] = input.nextInt();
 		}
 		System.out.println("\nUnsorted array");
-		// print array
+		// print unsorted array
 		for(int i = 0; i < array.length - 1; i++) {
 			System.out.println(array[i]);
 		}
