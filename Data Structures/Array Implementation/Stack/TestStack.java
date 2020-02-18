a4ebbe8a1881316a70c0094507cc5a1d82f729bc
Jason M <40440123+JMCSci@users.noreply.github.com>
2020-02-18 11:36:56 -0500

    Update TestStack.java

--- Data Structures/Array Implementation/Stack/TestStack.java
+++ Data Structures/Array Implementation/Stack/TestStack.java
@@ -4,18 +4,32 @@ package stack;
 
 public class TestStack {
 	public static void main(String[] args) {
-		MyStack<Integer> stack = new MyStack<>();
-		
+		MyStack<Integer> stack = new MyStack<>();	
 		System.out.println(stack.peek());
 		System.out.println(stack.isEmpty());
 		stack.push(1);
 		stack.push(2);
 		stack.push(3);
-		stack.push(4);
-		stack.push(5);
-		System.out.println(stack.isEmpty());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());	
 		System.out.println("SIZE: " + stack.size());
+		System.out.println(stack.getTop());
+		System.out.println(stack.getBottom());
+		System.out.println(stack.peek());
+		stack.push(2);
+		stack.push(5);
+		System.out.println("SIZE: " + stack.size());
+		System.out.println(stack.getTop());
+		System.out.println(stack.getBottom());
+		System.out.println(stack.pop());
+		System.out.println(stack.getTop());
 		System.out.println("PEEK: " + stack.peek());
+		System.out.println(stack.pop());
+		System.out.println(stack.getTop());
+		System.out.println(stack.getBottom());
+		System.out.println("PEEK: " + stack.peek());	
 	}
 
 }
