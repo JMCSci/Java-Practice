e5665dd4d3d6fb77b24171e27ed1e2b0386c3854
Jason M <40440123+JMCSci@users.noreply.github.com>
2020-02-18 08:19:55 -0500

    Update TestStack.java

--- Data Structures/Array Implementation/Stack/TestStack.java
+++ Data Structures/Array Implementation/Stack/TestStack.java
@@ -1,1 +1,21 @@
+/* Test implementation of a stack data structure */
 
+package stack;
+
+public class TestStack {
+	public static void main(String[] args) {
+		MyStack<Integer> stack = new MyStack<>();
+		
+		System.out.println(stack.peek());
+		System.out.println(stack.isEmpty());
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		stack.push(4);
+		stack.push(5);
+		System.out.println(stack.isEmpty());
+		System.out.println("SIZE: " + stack.size());
+		System.out.println("PEEK: " + stack.peek());
+	}
+
+}
