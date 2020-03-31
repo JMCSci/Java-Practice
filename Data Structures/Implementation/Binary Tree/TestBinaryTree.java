f97b8fed35aba0672c77e2498582b77b8d7781e5
Jason M <40440123+JMCSci@users.noreply.github.com>
2020-03-31 17:22:20 -0400

    Update TestBinaryTree.java

--- Data Structures/Implementation/Binary Tree/TestBinaryTree.java
+++ Data Structures/Implementation/Binary Tree/TestBinaryTree.java
@@ -5,6 +5,8 @@ package binarytree;
 public class TestBinaryTree {
 	public static void main(String[] args) {
 		Node tree = new Node(1);
+		boolean inTree = false;
+		int value = 4;
 		tree.insert(8);
 		tree.insert(4);
 		tree.insert(32);
@@ -15,6 +17,9 @@ public class TestBinaryTree {
 		tree.printPostorder();
 		System.out.println("\n### Preorder Traversal ###\n");
 		tree.printPreorder();
+		inTree = tree.contains(value);
+		System.out.println("\nIs the number " + value + " in the binary tree ? " + inTree);
+		
 	}
 	
 }
