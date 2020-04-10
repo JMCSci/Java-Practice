ca1d935af0cc148ab72123912534dec0114bda24
Jason M <40440123+JMCSci@users.noreply.github.com>
2020-04-10 16:19:02 -0400

    Update SinglyLinkedList.java

--- Data Structures/Implementation/Linked List/Singly Linked List/SinglyLinkedList.java
+++ Data Structures/Implementation/Linked List/Singly Linked List/SinglyLinkedList.java
@@ -1,4 +1,71 @@
-// removeFirst: Removes the first node in the list
+/* Singly Linked List */
+
+package linkedlist;
+
+public class SinglyLinkedList {
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		int num = 11;
+		for(int i = 1; i < 10; i++) {
+			list.insert(num * i);
+		}
+		list.traverse();
+		list.removeFirst();
+		System.out.println();
+		list.traverse();
+		list.removeLast();
+		System.out.println();
+		list.traverse();
+	}
+
+}
+
+class Node {
+	int data;
+	int index;				// create an index for this node
+	Node next;
+	
+	
+	Node(int data) {
+		this.data = data;
+	}
+	
+}
+
+class LinkedList {
+	Node head;
+	Node tail;
+	int size;
+	
+	LinkedList() {
+		
+	}
+	
+	// insert: Insert node into list
+	void insert(int data) {
+		Node node = new Node(data);
+		if(tail == null) {
+			head = tail = node;
+			size = 1;
+			node.index = size - 1;
+		} else {
+			tail.next = node;
+			tail = tail.next;
+			size++;
+			node.index = size - 1; 
+		}
+	}
+	
+	// traverse: Traverse list
+	void traverse() {
+		Node current = head;
+		while(current != null) {
+			System.out.println(current.data);
+			current = current.next;
+		}
+	}
+	
+	// removeFirst: Removes the first node in the list
 	int removeFirst() {
 		Node temp = head;
 		if(temp.next == null) {	
@@ -31,3 +98,10 @@
 		
 	}
 	
+	int size() {
+		return size;
+	}
+	
+	
+}
+
