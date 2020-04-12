9c052b85e878d597d3b2cfd12336e8be5e540d1a
Jason M <40440123+JMCSci@users.noreply.github.com>
2020-04-11 20:59:25 -0400

    Update Node.java

--- Data Structures/Implementation/Binary Tree/Node.java
+++ Data Structures/Implementation/Binary Tree/Node.java
@@ -124,6 +124,155 @@ public class Node {
 			right.printPreorder();
 		}
 	}
+	
+	// min: Returns the smallest number in the tree
+	int min() {
+		Node current = root;
+		if(current.left == null) {
+			return current.data;
+		} else {
+			while(current != null) {
+				if(current.left == null) {
+					break;
+				}
+				current = current.left;
+			}
+			return current.data;
+		}
+	
+	}
+	
+	// max: Returns the largest number in the tree
+	int max() {
+		Node current = root;
+		if(current.right == null) {
+			return current.data;
+		} else {
+			while(current != null) {
+				if(current.right == null) {
+					break;
+				}
+				current = current.right;
+			}
+			return current.data;	
+		}
+	}
+	
+	
+	void delete(int value) {
+		// Find node
+		Node current = root;		 			
+		Node parent = null;						// keep track of node to be removed parent 
+		while(current != null) {
+			if(value == current.data) {	
+				
+				break;							// stop checking when value is found - current node and parent node are now saved
+			} else {
+				parent = current;
+				if(value < current.data) {
+					current = current.left;
+				} else {
+					current = current.right;
+				}
+			}
+		}
+		
+		
+		
+		// CASE 1 -- Not in tree
+
+		// CASE 2 -- If no children -- LEAF
+		if(current.left == null && current.right == null) {
+			if(current.data < parent.data) {
+				parent.left = null;
+			} else {
+				parent.right = null;
+			}
+			
+		}
+		// CASE 3 -- If one child
+		else if(current.left == null || current.right == null) {
+			/* LEFT SIDE */
+			// Look to the left of parent -- b/c we are going need to connect to it -- need to know which side to connect
+			if(current.data < parent.data) {
+				// Data is less than parent data so we now know we have to connect to parents left
+				if(current.left != null) {
+				// If there is a left node then we do this	
+					parent.left = current.left;
+					current = null;
+				} else {
+				// If there is a right node	then we do this 
+					parent.left = current.right;
+					current = null;
+				}
+
+				/* RIGHT  SIDE */
+				// Look to the right of parent -- b/c we are going need know which side of parent to connect to
+			} else {
+				// We have to connect to parents right
+				if(current.right != null) {
+				// If there is a left node then we do this	
+					parent.right = current.right; 
+					current = null;
+				} else {
+				// If there is a right node	then we do this
+					parent.right = current.left;
+					current = null;
+				}
+
+			}
+			// CASE 4 -- If two children 
+			// Find leftmost child of right subtree -- go to right, then go as far left as possible
+		} else if(current.left != null && current.right != null) {
+			// We don't know which side current (child) is on parent -- this will verify
+			Node leftMost;
+			Node leftChild;			// temp hold left node child node of soon to be deleted node
+			Node rightChild;		// temp hold right child node of soon to be deleted node
+			/* LEFT SIDE */
+			if(current.data < parent.data) {
+				rightChild = current.right;
+				// Check if there is a right node
+				if(current.right != null) {
+					leftMost = current.right;
+					// Find left most node -- go as far left as possible
+					while(leftMost.left != null) {
+						leftMost = current.left;
+					}
+				} else {
+					// There isn't a left node -- we just take the right node instead
+					leftMost = current.right;
+				}
+				parent.right = rightChild;
+				parent.left = leftMost;
+				current = null;
+			/* RIGHT SIDE */
+			} else {
+				leftChild = current.left;
+				if(current.right != null) {
+					leftMost = current.right;
+					// Find left most node -- go as far left as possible 
+					while(leftMost.left != null) {
+						leftMost = leftMost.left; 
+					}
+				} else {
+					// There isn't a left node -- we just take the right node instead
+					leftMost = current.right;
+				}
+				parent.left = leftChild;
+				System.out.println(parent.left.data);
+				parent.right = leftMost;
+				current = null;
+			}
+			
+			
+			
+			
+		}
+		
+		
+			
+	
+	}
 
 
 
