41934220eb13b7578c6073074189070e5c8dea40
Jason M <40440123+JMCSci@users.noreply.github.com>
2020-09-02 15:51:48 -0400

    Create UpdatedBST.java

---
+++ Data Structures/Implementation/Binary Tree/UpdatedBST.java
@@ -0,0 +1,84 @@
+package bst1;
+
+import java.util.Stack;
+
+public class TreeNode {
+	int data;
+	int size;
+	TreeNode root;
+	TreeNode left;
+	TreeNode right;
+	
+	TreeNode(int data){
+		this.data = data;
+		size = 1;
+		if(root == null) {
+			root = this;
+		}
+	}
+	
+	void insert(int value) {
+		TreeNode node = new TreeNode(value);
+		TreeNode current = root;
+		
+		while(current != null) {
+			if(value < data && current.left != null) {
+				current = current.left;
+			} else if(value > data && current.right != null) {
+				current = current.right;
+			} else {
+				break;
+			}
+		}
+		
+		if(value < current.data && current.left == null) {
+			current.left = node;
+		} else if(value > current.data && current.right == null) {
+			current.right = node;
+		}
+		size++;
+	}
+	
+	void inorder() {
+		Stack<TreeNode> stack = new Stack<>();
+		TreeNode current = root;
+		
+		while(!stack.isEmpty() || current != null) {
+			if(current != null) {
+				stack.push(current);
+				current = current.left;
+			} else {
+				current = stack.pop();
+				System.out.println(current.data);
+				current = current.right;
+			}
+		}
+	}
+	
+	void inorderRecursive() {
+		TreeNode current = root;
+		if(current.left != null) {
+			left.inorderRecursive();
+		}
+		System.out.println(data);
+		if(current.right != null) {
+			right.inorderRecursive();
+		}
+	}
+	
+	boolean contains(int value) {
+		TreeNode current = root;
+		while(current != null) {
+			if(value == current.data) {
+				return true;
+			} else if(value < current.data) {
+				current = current.left;
+			} else if(value > current.data) {
+				current = current.right;
+			}
+		}
+		return false;
+	}
+	
+
+}
